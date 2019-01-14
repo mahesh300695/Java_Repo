b097a28dcfa55fd8b69e6338c9d68d97a0ff3d19
mahesh300695 <46525873+mahesh300695@users.noreply.github.com>
2019-01-14 19:33:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Welcome to GIT");
+		System.out.println("Modified to run automatically");
 	}
 }
